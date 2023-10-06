40c030ae74c3472f3377bf2d1ec54346f1851160
CodeWithIsmail <hmismail360@gmail.com>
2023-10-06 21:09:13 +0600

    course class created

--- StudyMate/src/Main.java
+++ StudyMate/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Student student1=new Student("ismail","hossain","CodeWithIsmail","ismail@gmail.com","123");
+        Student student1 = new Student("ismail", "hossain", "CodeWithIsmail", "ismail@gmail.com", "123");
         System.out.println(student1.toString());
+        Course NE = new Course("Numerical Analysis", "MATH 304","a",3,1,2);
+        System.out.println(NE.toString());
     }
 }
